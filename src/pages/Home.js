2730a0b2312a9000be30d008cd3f714137292efc
Hasan Shanto <hasanshanto922@gmail.com>
2022-12-03 12:31:32 +0600

    first commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,35 +1,23 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const {
-    state: { products, loading, error },
-  } = useProducts();
+  const [products, setProducts] = useState([]);
 
-  let content;
-
-  if (loading) {
-    content = <p>Loading</p>;
-  }
-
-  if (error) {
-    content = <p>Something went wrong</p>;
-  }
-
-  if (!loading && !error && products.length === 0) {
-    content = <p>Nothing to show, product list is empty</p>;
-  }
-
-  if (!loading && !error && products.length) {
-    content = products.map((product) => (
-      <ProductCard key={product._id} product={product} />
-    ));
-  }
+  useEffect(() => {
+    fetch('products.json')
+    .then(res => res.json())
+    .then(data => setProducts(data))
+  }, [])
+  
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      {content}
+      {
+        products.map(product => <ProductCard
+        product={product}></ProductCard>)
+      }
     </div>
   );
 };
